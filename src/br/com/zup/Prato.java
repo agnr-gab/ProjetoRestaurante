65086581c00613249a46a08f9bfe6c4570479a6f
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-28 23:19:31 -0300

    Classe Prato: Lista criada e metodo add ingredientes

--- src/br/com/zup/Prato.java
+++ src/br/com/zup/Prato.java
@@ -1,5 +1,12 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Prato {
-    Li
+    List<Ingredientes> listaIngrediente = new ArrayList<>();
+
+    public void adicionarIngrediente(Ingredientes acompanhamentos){
+        listaIngrediente.add(acompanhamentos);
+    }
 }

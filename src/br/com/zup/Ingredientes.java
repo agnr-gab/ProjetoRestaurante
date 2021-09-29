9aab1ee7bb2f7c22c5679457731cc83edce9d6f7
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-29 00:28:31 -0300

    Classe Ingredientes: metodos acessores e toString()

--- src/br/com/zup/Ingredientes.java
+++ src/br/com/zup/Ingredientes.java
@@ -1,4 +1,26 @@
 package br.com.zup;
 
 public class Ingredientes {
+        private String escolhaIngredientes;
+
+        public Ingredientes() {
+        }
+
+        public Ingredientes(String escolhaIngredientes) {
+                this.escolhaIngredientes = escolhaIngredientes;
+        }
+
+        public String getEscolhaIngredientes() {
+                return escolhaIngredientes;
+        }
+
+        public void setEscolhaIngredientes(String escolhaIngredientes) {
+                this.escolhaIngredientes = escolhaIngredientes;
+        }
+        @Override
+        public String toString() {
+                StringBuilder retornoIngredientes = new StringBuilder();
+                retornoIngredientes.append(escolhaIngredientes);
+                return retornoIngredientes.toString();
+        }
 }

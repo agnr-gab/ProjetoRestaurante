f64436e413e71737e560113a7d636f481359e741
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-29 01:02:37 -0300

    Classe Sistema: iniciada

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,5 +1,20 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
 
+    private static Scanner entradaDados(String prato) {
+        System.out.println(prato);
+        return new Scanner(System.in);
+    }
+
+    public static void pratoDia() {
+
+        System.out.println("___Seja bem vinde ao sistema de cadastro de pratos do dia do restaurante Coma Bem!___");
+        String nomePrato = entradaDados("Digite o nome do prato que deseja cadastrar:").nextLine();
+        double valorPrato = entradaDados("Digite o preço do prato:").nextDouble();
+
+
+    }
 }

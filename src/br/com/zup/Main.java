c2832845abbc26c44982bcc7b0d065e679267674
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-30 12:47:36 -0300

    Acrescentar o sistema no Main

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -3,8 +3,8 @@ package br.com.zup;
 public class Main {
 
     public static void main(String[] args) {
-	Prato prato1 = new Prato("Strogonoff Vegano", 35);
-	Ingredientes acompanhamentos1 = new Ingredientes("Batata palha");
+        Sistema.cadastrarpratoDia();
+        Sistema.retornoIngredientes();
 
     }
 }
